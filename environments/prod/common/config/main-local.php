8d9e7aa21ba2fcf1479418110a2537fa61e87e61
Vitalii <prodan.vitaliy@gmail.com>
2016-05-05 23:08:24 +0300

    changed environment config

--- environments/prod/common/config/main-local.php
+++ environments/prod/common/config/main-local.php
@@ -1,11 +1,12 @@
 <?php
+$host = "mysql://$OPENSHIFT_MYSQL_DB_HOST:$OPENSHIFT_MYSQL_DB_PORT/";
 return [
     'components' => [
         'db' => [
             'class' => 'yii\db\Connection',
-            'dsn' => 'mysql:host=localhost;dbname=yii2advanced',
-            'username' => 'root',
-            'password' => '',
+            'dsn' => "mysql:host=$host;dbname=php",
+            'username' => 'adminEuvydx4',
+            'password' => 'vMv6F7R88AR2',
             'charset' => 'utf8',
         ],
         'mailer' => [
